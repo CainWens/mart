6940325dfbaf213bfaec058b48411838ab1aa4c9
CainWens <makss314@gmail.com>
2024-03-08 00:34:19 +0700

    com 2

--- resources/js/mart.js
+++ resources/js/mart.js
@@ -1,8 +1,6 @@
 import { MathUtils, Vector2, Vector3, CatmullRomCurve3 } from "https://esm.sh/three";
 import * as TWEEN from "https://esm.sh/three/addons/libs/tween.module.js";
 
-
-
 let uY = (val) => cnv.height * 0.01 * val;
 let uX = (val) => cnv.width * 0.01 * val;
 
